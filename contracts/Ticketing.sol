f0bad64238c82458f647dea9a1d79e01fd03498c
Victor Yunenko <victor@what.digital>
2019-06-21 19:35:05 +0200

    add price setˆ

--- contracts/Ticketing.sol
+++ contracts/Ticketing.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.1;
 pragma experimental ABIEncoderV2;
 
 contract Ticketing {
-    
+
     struct Trip {
         uint startTimestamp;
         uint endTimestamp;
@@ -12,14 +12,14 @@ contract Ticketing {
         bool isPaid;
         uint price;
     }
-    
+
     struct Passenger {
         bool isCheckedIn;
         address checkedInTspKey;
         Trip[] trips;
     }
-    
-    mapping (address => Passenger) public passengers;
+
+    mapping(address => Passenger) public passengers;
 
     event TripCreated(
         uint startTimestamp,
@@ -30,12 +30,20 @@ contract Ticketing {
         bool isPaid,
         uint price
     );
-    
+
     event CheckedOut(
+        uint tripIndex,
         uint startTimestamp,
         uint endTimestamp,
         address transporter,
         address passenger
+    );
+
+    event TripPriceSet(
+        uint tripIndex,
+        uint price,
+        address transporterAddress,
+        address passengerAddress
     );
 
     constructor() public {
@@ -45,13 +53,13 @@ contract Ticketing {
         address transporterAddress
     ) public {
         Trip memory trip = Trip({
-            startTimestamp: now,
-            endTimestamp: 0,
-            transporter: transporterAddress,
-            passenger: msg.sender,
-            isCheckedOut: false,
-            isPaid: false,
-            price: 0
+            startTimestamp : now,
+            endTimestamp : 0,
+            transporter : transporterAddress,
+            passenger : msg.sender,
+            isCheckedOut : false,
+            isPaid : false,
+            price : 0
         });
         passengers[msg.sender].trips.push(trip);
         passengers[msg.sender].isCheckedIn = true;
@@ -67,38 +75,65 @@ contract Ticketing {
         );
     }
 
-    function getTrips(address passengerAddress) public view returns(Trip[] memory) {
+    function getTrips(address passengerAddress) public view returns (Trip[] memory) {
         return passengers[passengerAddress].trips;
     }
-    
+
     function checkOut() public {
         Passenger storage passenger = passengers[msg.sender];
         Trip[] memory trips = passenger.trips;
         Trip storage trip = passenger.trips[trips.length - 1];
         trip.isCheckedOut = true;
         trip.endTimestamp = now;
-        
+
         passenger.isCheckedIn = false;
         passenger.checkedInTspKey = address(0);
-        
+
+        uint tripIndex = trips.length - 1;
         emit CheckedOut(
+            tripIndex,
             trip.startTimestamp,
             trip.endTimestamp,
             trip.transporter,
             trip.passenger
         );
-        
+
     }
-//
-//    function setPrice(address passengerAddress, unit price) public {
-//        Passenger storage passenger = passengers[passengerAddress];
-//        tripLast = passenger.trips;
-//        
-//        memory 
-//        
-//        for (uint i = passenger.trips.leng; i<studentList.length; i++) {
-//            emit LogStudentGrade(studentList[i], studentStructs[studentList[i]].grade);
-//        }
-//
-//    }
+
+    function setPrice(
+        address passengerAddress,
+        uint price,
+        uint tripStartTimestamp
+    ) public {
+        Passenger storage passenger = passengers[passengerAddress];
+
+        uint tripIndexLast = passenger.trips.length - 1;
+
+        for (uint i = tripIndexLast; i > 0; i--) {
+            Trip storage trip = passenger.trips[i];
+            bool isTripMatched = (
+                trip.isCheckedOut &&
+                !!trip.isPaid &&
+                trip.startTimestamp == tripStartTimestamp
+            );
+            if (isTripMatched) {
+                Trip storage tripUnpaid = trip;
+                bool isHasNoPermissionToSetPrice = msg.sender != trip.transporter;
+                if (isHasNoPermissionToSetPrice) {
+                    revert("only TSP set the price");
+                }
+                tripUnpaid.price = price;
+                
+                uint tripIndex = i;
+                address transporterAddress = msg.sender;
+                emit TripPriceSet(
+                    tripIndex,
+                    price,
+                    transporterAddress,
+                    passengerAddress
+                );
+                break;
+            }
+        }
+    }
 }
